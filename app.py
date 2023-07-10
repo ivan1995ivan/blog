4ea3ac2a01e3bd629678d866bc5e4e5f0c5aa3da
ivan1995ivan <111653497+ivan1995ivan@users.noreply.github.com>
2023-07-10 10:24:07 +0300

    create_article

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
@@ -14,9 +15,11 @@ class Article(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return 'Article %r' % self.id
+        return '<Article %r>' % self.id
+
 
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html')
 
@@ -26,9 +29,24 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name,id):
-    return 'User page: ' + name +'-'+ str(id)
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'При добавлении статьи произошла ошибка'
+    else:
+        return render_template('create-article.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
